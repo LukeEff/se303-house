105ece46d7cf7e6ef5938e37d2d7b75f41c6f4c1
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-18 16:29:16 -0700

    housenumber: copy methods from house

--- lib/house.rb
+++ lib/house.rb
@@ -115,4 +115,100 @@ class HouseNumber
     @number = number
   end
 
+  def house(number)
+    if number <= 0
+      ""
+    else
+      "the house that Jack built"
+    end
+  end
+
+  def malt(number)
+    if number <= 1
+      ""
+    else
+      "the malt that lay in "
+    end
+  end
+
+  def rat(number)
+    if number <= 2
+      ""
+    else
+      "the rat that ate "
+    end
+  end
+  
+  def cat(number)
+    if number <= 3
+      ""
+    else
+      "the cat that killed "
+    end
+  end
+
+  def dog(number)
+    if number <= 4
+      ""
+    else
+      "the dog that worried "
+    end
+  end
+
+  def cow(number)
+    if number <= 5
+      ""
+    else
+      "the cow with the crumpled horn that tossed "
+    end
+  end
+
+  def maiden(number)
+    if number <= 6
+      ""
+    else
+      "the maiden all forlorn that milked "
+    end
+  end
+
+  def man(number)
+    if number <= 7
+      ""
+    else
+      "the man all tattered and torn that kissed "
+    end
+  end
+
+  def priest(number)
+    if number <= 8
+      ""
+    else
+      "the priest all shaven and shorn that married " 
+    end
+  end
+
+  def rooster(number)
+    if number <= 9
+      ""
+    else
+      "the rooster that crowed in the morn that woke "
+    end
+  end
+
+  def farmer(number)
+    if number <= 10
+      ""
+    else
+      "the farmer sowing his corn that kept "
+    end
+  end
+
+  def horse(number)
+    if number <= 11
+      ""
+    else
+      "the horse and the hound and the horn that belonged to "
+    end
+  end
+  
 end
