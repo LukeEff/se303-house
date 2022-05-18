6da47bbd13427bfe2eb57757c308ea8f6982ad4e
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-18 16:05:23 -0700

    house: replace malt excerpt with method interpolation

--- lib/house.rb
+++ lib/house.rb
@@ -4,7 +4,7 @@ class House
     when 1  
       "This is the house that Jack built.\n"
     else 
-      "This is #{horse(number)}#{farmer(number)}#{rooster(number)}#{priest(number)}#{man(number)}#{maiden(number)}#{cow(number)}#{dog(number)}#{cat(number)}#{rat(number)}the malt that lay in the house that Jack built.\n"
+      "This is #{horse(number)}#{farmer(number)}#{rooster(number)}#{priest(number)}#{man(number)}#{maiden(number)}#{cow(number)}#{dog(number)}#{cat(number)}#{rat(number)}#{malt(number)}the house that Jack built.\n"
     end
   end
 
