744ec2c9fa235539171aa2bab2badc5ab3425605
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-18 17:40:14 -0700

    house: remove unused action and noun methods

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,5 @@
 class House
+  
   def line(number)
     house_component = HouseComponent.new(number)
     phrase = "This is "
@@ -8,19 +9,10 @@ class House
     phrase << ".\n"
   end
 
-  def noun(number) 
-    nouns = ["house","malt","rat","cat","dog", "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
-    nouns[number]
-  end
-
-  def action(number)
-    actions = ["Jack built", "lay in ", "ate ", "killed ", "worried ", "tossed ", "milked ", "kissed ", "married ", "woke ", "kept ", "belonged to "]
-    actions[number]
-  end
-
   def recite
     (1..12).map { |number| line(number) }.join("\n")
   end
+
 end
 
 class HouseComponent
