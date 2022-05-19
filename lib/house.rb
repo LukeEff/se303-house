2d44bdb118de16ad47081dc1679f349b4440e3b6
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-18 17:19:05 -0700

    house: create method action

--- lib/house.rb
+++ lib/house.rb
@@ -6,9 +6,23 @@ class House
     phrase << ".\n"
   end
 
+  def templine(number)
+    
+    phrase = "This is "
+
+    for i in 1..number do
+      phrase << "the #{noun(number)} that #{action(number)}"
+    end
+
+  end
+
   def noun(number) 
     nouns = ["house","malt","rat","cat","dog", "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn",  "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
     nouns[number]
+  end
+
+  def action(number)
+    actions = ["Jack built", "lay in ", "ate ", "killed ", "worried ", "tossed ", "milked ", "kissed ", "married ", "woke ", "kept ", "belonged to "]
   end
 
   def house(number)
